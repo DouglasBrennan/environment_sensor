9f03ba4720544af95962775159af6efc274f9bff
DouglasBrennan <douglas.brennan@me.com>
2022-09-07 21:04:30 +0200

    adapt light and proximity reading

--- raspi_sensor.py
+++ raspi_sensor.py
@@ -3,7 +3,7 @@ import time
 from smbus2 import SMBus
 from bme280 import BME280
 from enviroplus import gas
-import ltr559
+from ltr559 import LTR559
 
 from interface import Sensor, Reading, Weather, Light, Location
 
@@ -12,16 +12,18 @@ class RaspiSensor(Sensor):
     def __init__(self):
         self.bus = SMBus(1)
         self.bme280 = BME280(i2c_dev=self.bus)
+        self.ltr559 = LTR559()
 
     def get_reading(self) -> Reading:
+        self.ltr559.update_sensor()
         weather = Weather(
             temperature=self.bme280.get_temperature(),
             pressure=self.bme280.get_pressure(),
             humidity=self.bme280.get_humidity(),
         )
         light = Light(
-            lux=ltr559.LTR559.get_lux(),
-            proximity=ltr559.LTR559.get_proximity(),
+            lux=self.ltr559.get_lux(),
+            proximity=self.ltr559.get_proximity(),
         )
         location = Location(
             latitude=47.37689,
